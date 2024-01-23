75cc7f38bea1345a0890e64c4fd116cd5560eb00
unknown <charleseduardofaria@gmail.com>
2024-01-23 10:06:58 -0300

    Criando e configurando propriedades do projeto

---
+++ src/test/java/br/com/charlesedu/demoajax/DemoAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.charlesedu.demoajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
