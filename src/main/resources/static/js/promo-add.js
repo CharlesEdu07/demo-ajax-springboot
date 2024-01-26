add567594c61aa9e429433bcd8f026108ac669af
unknown <charleseduardofaria@gmail.com>
2024-01-26 10:48:12 -0300

    Recuperando dados de url através de requisição ajax

---
+++ src/main/resources/static/js/promo-add.js
@@ -0,0 +1,14 @@
+$("#saleLink").on('change', function () {
+    var url = $(this).val();
+
+    if (url.length > 7) {
+        $.ajax({
+            method: "POST",
+            url: "meta/info?url=" + url,
+            cache: false,
+            success: function (data) {
+                console.log(data);
+            },
+        });
+    }
+});
