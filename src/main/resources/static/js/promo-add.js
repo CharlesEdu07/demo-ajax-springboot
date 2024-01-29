60cafc85146d458e38856c8b393593962febf5e8
unknown <charleseduardofaria@gmail.com>
2024-01-29 11:12:45 -0300

    Loading e limpando campos ao atualizar url

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -6,6 +6,13 @@ $("#saleLink").on('change', function () {
             method: "POST",
             url: "meta/info?url=" + url,
             cache: false,
+            beforeSend: function () {
+                $("#alert").removeClass("alert alert-danger").text("");
+                $("#title").val("");
+                $("#site").text("");
+                $("#imageLink").attr("src", "");
+                $("#loader-img").addClass("loader");
+            },
             success: function (data) {
                 console.log(data);
 
@@ -16,10 +23,15 @@ $("#saleLink").on('change', function () {
             statusCode: {
                 404: function () {
                     $("#alert").addClass("alert alert-danger").text("Nenhuma informação pode ser recuperada da URL informada.");
+                    $("#imageLink").attr("src", "/images/promo-dark.jpg");
                 }
             },
             error: function (data) {
                 $("#alert").addClass("alert alert-danger").text("Algo deu errado. Tente novamente mais tarde.");
+                $("#imageLink").attr("src", "/images/promo-dark.jpg");
+            },
+            complete: function () {
+                $("#loader-img").removeClass("loader");
             }
         });
     }
