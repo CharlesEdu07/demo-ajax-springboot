5ab18df0c01d19220ae1be52ad69d93c044433a3
unknown <charleseduardofaria@gmail.com>
2024-02-02 10:42:44 -0300

    SaleController e listando Categorias no combo-box

---
+++ src/main/java/br/com/charlesedu/demoajax/web/controller/SaleController.java
@@ -0,0 +1,30 @@
+package br.com.charlesedu.demoajax.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.charlesedu.demoajax.domain.Category;
+import br.com.charlesedu.demoajax.repository.CategoryRepository;
+
+@Controller
+@RequestMapping("/sale")
+public class SaleController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @ModelAttribute("categories")
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/add")
+    public String openRegistration() {
+        return "promo-add";
+    }
+}
