23784598563c04744952c62d4a4ba6d68d855173
unknown <charleseduardofaria@gmail.com>
2024-02-20 15:32:50 -0300

    Métodos para update e consulta de likes

--- src/main/java/br/com/charlesedu/demoajax/repository/SaleRepository.java
+++ src/main/java/br/com/charlesedu/demoajax/repository/SaleRepository.java
@@ -1,9 +1,20 @@
 package br.com.charlesedu.demoajax.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.charlesedu.demoajax.domain.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Transactional(readOnly = false)
+    @Modifying
+    @Query("UPDATE Sale s SET s.likes = s.likes + 1 WHERE s.id = :id")
+    void sumLikes(@Param("id") Long id);
+
+    @Query("SELECT s.likes FROM Sale s WHERE s.id = :id")
+    int findLikesById(@Param("id") Long id);
 }
