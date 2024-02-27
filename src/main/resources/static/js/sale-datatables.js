797770e791fa98fc4e32e3a71569cc2156238a4c
unknown <charleseduardofaria@gmail.com>
2024-02-27 08:32:25 -0300

    Preparando datatable no lado cliente

---
+++ src/main/resources/static/js/sale-datatables.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("table-server").DataTable({
+        processing: true,
+        serverSide: true,
+        responsive: true,
+        lengthMenu: [10, 15, 20, 25],
+        ajax: {
+            url: "/sale/datatables/server",
+            data: "data",
+        },
+        columns: [
+            { data: "id" },
+            { data: "title" },
+            { data: "site" },
+            { data: "saleLink" },
+            { data: "description" },
+            { data: "imageLink" },
+            { data: "price" },
+            { data: "likes" },
+            { data: "registerDate" },
+            { data: "category" }
+        ]
+    });
+});
